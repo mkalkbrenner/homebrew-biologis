adc7bca5415d6a8cedb416dc67309f2503f5fe84
Christian Spitzlay <christian.spitzlay@bio.logis.de>
2015-04-13 15:18:16 +0200

    move hash value from md5 to sha256

--- Formula/fakemail-python.rb
+++ Formula/fakemail-python.rb
@@ -3,7 +3,7 @@ require 'formula'
 class FakemailPython < Formula
   homepage 'http://www.lastcraft.com/fakemail.php'
   url 'http://downloads.sourceforge.net/project/fakemail/fakemail-python/1.0/fakemail-python-1.0.tar.gz'
-  md5 'a9501bc6a4045ff7e67ad5017a2bb06f'
+  sha256 'c7bbc1b1dd3810a9fb615e422ffb0deb4977e9075d2baf1028001c2ff569b8c0'
 
   def install
     system "python", "setup.py", "install", "--prefix=#{prefix}" 
