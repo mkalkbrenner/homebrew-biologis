f8e8266a82bc2eebfb65b04f645bb281a6bcf7c4
Markus Kalkbrenner <markus.kalkbrenner@bio.logis.de>
2015-12-01 19:13:40 +0100

    2.0.9

--- Formula/mod_perl.rb
+++ Formula/mod_perl.rb
@@ -5,14 +5,16 @@ require 'formula'
 
 class ModPerl < Formula
   homepage 'http://perl.apache.org/'
-  url 'http://archive.apache.org/dist/perl/mod_perl-2.0.8.tar.gz'
-  version '2.0.8'
-  sha1 '165b1062f9680d1ccc92a88f1699797b6860c499'
+  url 'http://archive.apache.org/dist/perl/mod_perl-2.0.9.tar.gz'
+  version '2.0.9'
+  sha1 '5de3018214da21de186d758a429d5c78f827caa5'
+
+  depends_on :apr => :build
 
   def install
     ENV.j1  # if your formula's build system can't parallelize
-
-    system "perl", "Makefile.PL", "MP_APXS=/usr/local/bin/apxs", "MP_CCOPTS=-std=gnu89", "DESTDIR=#{prefix}"
+    
+    system "perl", "Makefile.PL", "MP_APXS=/usr/local/bin/apxs", "MP_APR_CONFIG=#{Formula["apr"].opt_prefix}/bin/apr-1-config", "MP_CCOPTS=-std=gnu89", "DESTDIR=#{prefix}"
     system "make"
     system "make install"
   end
